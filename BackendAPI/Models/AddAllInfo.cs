2a9e74db5dd9760b77d40c24d3b985355a144c08
Aakash Sharma <aakashsharma7232@gmail.com>
2024-02-03 14:04:57 -0500

    Added USser info Update and delete api and Update api for organisation. Added Pincode also

--- BackendAPI/Models/AddAllInfo.cs
+++ BackendAPI/Models/AddAllInfo.cs
@@ -14,5 +14,6 @@
         public string Type { get; set; }
        // public string Username { get; set; }
         public string Password { get; set; }
+        public int PinCode { get; set; }
     }
 }
