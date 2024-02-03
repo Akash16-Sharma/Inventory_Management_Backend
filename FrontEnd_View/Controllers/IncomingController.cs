52c989d130ba6cbe22c7009ecfa776aa88933f80
Prateek Dutt <prateekdutt4190@gmail.com>
2024-02-04 00:01:11 +0530

    incomin cntrl -vendor CRUD, item,category add pagination layout add datatable code

---
+++ FrontEnd_View/Controllers/IncomingController.cs
@@ -0,0 +1,93 @@
+﻿using BackendAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace FrontEnd_View.Controllers
+{
+    public class IncomingController : Controller
+    {
+
+        //this cntroler is for incoming order and vendor details 
+        Uri baseAddress = new Uri("http://localhost:39496/api");
+        private readonly HttpClient _client;
+
+        public IncomingController()
+        {
+            _client =new HttpClient();
+            _client.BaseAddress = baseAddress;
+        }
+
+        public IActionResult Index() //for showing vendor list 
+        {
+            int OrgId = HttpContext.Session.GetInt32("orgId") ?? 0;
+            int StaffId = HttpContext.Session.GetInt32("staffId") ?? 0;
+            List<Vendor> vendor = new List<Vendor>();
+            HttpResponseMessage responseMessage = _client.GetAsync(_client.BaseAddress +
+                "/Vendor/Get?OrgId=" + OrgId + "&StaffId=" + StaffId).Result;
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                string data = responseMessage.Content.ReadAsStringAsync().Result;
+                vendor = JsonConvert.DeserializeObject<List<Vendor>>(data);
+            }
+
+            return View(vendor);
+
+        }
+
+        public IActionResult VendorAdd(Vendor ven) //vendor add
+        {
+            int OrgId = HttpContext.Session.GetInt32("orgId") ?? 0;
+            int StaffId = HttpContext.Session.GetInt32("staffId") ?? 0;
+            ven.OrgId = OrgId;
+            string data = JsonConvert.SerializeObject(ven);
+            StringContent con = new StringContent(data, Encoding.UTF8, "application/json");
+            HttpResponseMessage responseMessage = _client.PostAsync(_client.BaseAddress +
+                "/Vendor/AddVendor?StaffId=" + StaffId, con).Result;
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public IActionResult VendorEdit(Vendor ven) //vendor edit
+        {
+            int StaffId = HttpContext.Session.GetInt32("staffId") ?? 0;
+            int OrgId = HttpContext.Session.GetInt32("orgId") ?? 0;
+            ven.OrgId = OrgId;
+            string data = JsonConvert.SerializeObject(ven);
+            StringContent con = new StringContent(data, Encoding.UTF8, "application/json");
+            HttpResponseMessage responseMessage = _client.PutAsync(_client.BaseAddress +
+                "/Vendor/UpdateVendor?StaffId=" + StaffId, con).Result;
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public IActionResult VendorDelete(int Id)  //vendor delete
+        {
+            int StaffId = HttpContext.Session.GetInt32("staffId") ?? 0;
+            HttpResponseMessage responseMessage = _client.DeleteAsync(_client.BaseAddress +
+               "/Vendor/DeleteVendor?id=" + Id + "&StaffId=" + StaffId).Result;
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        } 
+
+        public IActionResult IncOrders()  //show orders
+        {
+            return View();
+        }
+
+    }
+}
