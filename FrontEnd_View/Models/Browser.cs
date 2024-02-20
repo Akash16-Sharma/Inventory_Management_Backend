21abc9ebfe0c7dbe9c281c36d01b6b066482bd36
Prateek Dutt <prateekdutt4190@gmail.com>
2024-02-20 16:41:28 +0530

    check mobile func add, incorder mobile view done

---
+++ FrontEnd_View/Models/Browser.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+namespace FrontEnd_View.Models
+{
+    public class Browser
+    {
+        public string userAgent { get; set; }
+        public Regex OS { get; set; }
+        public Regex device { get; set; }
+    }
+}
