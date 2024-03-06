8b9891edf480c63b3ff03cd07f5a33d7f8be74c1
Aakash Sharma <aakashsharma7232@gmail.com>
2024-03-06 14:38:00 -0500

    Sending Email While Adding Role To The User

--- BackendAPI/Models/EmailModel.cs
+++ BackendAPI/Models/EmailModel.cs
@@ -3,6 +3,7 @@
     public class EmailModel
     {
         public string To { get; set; }
+        public string From { get; set; }
         public string Subject { get; set; }
         public string Body { get; set; }
     }
--- BackendAPI/Repository/EmailRepo.cs
+++ BackendAPI/Repository/EmailRepo.cs
@@ -10,13 +10,17 @@ namespace BackendAPI.Repository
     {
         public async Task<bool> SendEmailAsync(EmailModel emailModel)
         {
+            if(emailModel.From==null)
+            {
+                emailModel.From= "ninjainventory@gmail.com";
+            }
             try
             {
                 using (var client = new SmtpClient())
                 {
                     var credentials = new NetworkCredential("ninjainventory@gmail.com", "tdjelnusbtibtomj"); // Replace with your email credentials
                     client.Host = "smtp.gmail.com"; // Replace with your SMTP server host
-                    client.Port = 465; // Replace with your SMTP port
+                    client.Port = 587; // Replace with your SMTP port
                     client.EnableSsl = true; // Enable SSL if required
 
                     client.UseDefaultCredentials = false;
@@ -24,7 +28,7 @@ namespace BackendAPI.Repository
 
                     var message = new MailMessage
                     {
-                        From = new MailAddress("ninjainventory@gmail.com"), // Replace with your email address
+                        From = new MailAddress(emailModel.From), // Replace with your email address
                         Subject = emailModel.Subject,
                         Body = emailModel.Body,
                         IsBodyHtml = true
